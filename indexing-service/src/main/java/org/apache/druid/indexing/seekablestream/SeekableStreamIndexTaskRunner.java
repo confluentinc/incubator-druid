b534c4bce92da3931c0831ba59e9beb52bb12bf1
Sadananda Aithal <saithal@confluent.io>
2024-04-09 16:28:32 +0530

    RCCA-17777 Prevent checkpointed sequence from getting checkpointed again

--- indexing-service/src/main/java/org/apache/druid/indexing/seekablestream/SeekableStreamIndexTaskRunner.java
+++ indexing-service/src/main/java/org/apache/druid/indexing/seekablestream/SeekableStreamIndexTaskRunner.java
@@ -729,7 +729,12 @@ public abstract class SeekableStreamIndexTaskRunner<PartitionIdType, SequenceOff
           }
 
           if (System.currentTimeMillis() > nextCheckpointTime) {
+<<<<<<< Updated upstream
             sequenceToCheckpoint = getLastSequenceMetadata();
+=======
+            sequenceToCheckpoint = getLastSequenceMetadata().isCheckpointed() ? null : getLastSequenceMetadata();
+            log.info("Next checkpoint time, updating sequenceToCheckpoint, SequenceToCheckpoint: [%s]", sequenceToCheckpoint);
+>>>>>>> Stashed changes
           }
 
           if (sequenceToCheckpoint != null && stillReading) {
