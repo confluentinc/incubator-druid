apache/druid

Removing extra changes and minor fix

processing/src/main/java/org/apache/druid/java/util/http/client/pool/MetricsEmittingResourcePool.java
@@ -38,7 +38,7 @@ public class MetricsEmittingResourcePool<K, V> extends ResourcePool<K, V>
     long startTime = System.nanoTime();
     ResourceContainer<V> retVal = super.take(key);
     long totalduration = System.nanoTime() - startTime;
-    emitter.emit(ServiceMetricEvent.builder().setDimension("destination", key.toString()).setMetric("httpClient/channelAcquire/time", totalduration));
+    emitter.emit(ServiceMetricEvent.builder().setDimension("server", key.toString()).setMetric("httpClient/channelAcquire/time", totalduration));
     return retVal;
   }
 

confluentinc/incubator-druid

core/src/main/java/org/apache/druid/java/util/http/client/pool/MetricsEmittingResourcePool.java
@@ -38,7 +38,7 @@ public class MetricsEmittingResourcePool<K, V> extends ResourcePool<K, V>
     long startTime = System.nanoTime();
     ResourceContainer<V> retVal = super.take(key);
     long totalduration = System.nanoTime() - startTime;
-    emitter.emit(ServiceMetricEvent.builder().setDimension("destination", key.toString()).build("httpClient/channelAcquire/time", totalduration));
+    emitter.emit(ServiceMetricEvent.builder().setDimension("server", key.toString()).build("httpClient/channelAcquire/time", totalduration));
     return retVal;
   }
 